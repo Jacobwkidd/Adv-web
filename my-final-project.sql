5f0d152a9a95c905f8dd803db447ef8e6eb8fed9
Jacob Kidd <kiddj1@students.westerntc.edu>
2023-10-09 12:52:47 -0500

    final project

--- my-final-project.sql
+++ my-final-project.sql
@@ -22,7 +22,7 @@ CREATE TABLE users (
   user_email varchar(100) NOT NULL UNIQUE,
   user_password char(255) NOT NULL,
   user_salt char(32) NOT NULL,
-  user_role INT NOT NULL DEFAULT '1',
+  user_role INT NOT NULL,
   user_active boolean NOT NULL DEFAULT true,
   FOREIGN KEY (user_role) REFERENCES user_roles(user_role_id)
 );
@@ -33,15 +33,15 @@ CREATE TABLE landlord (
   landlord_user_last_name varchar(30) NOT NULL, 
   landlord_user_email varchar(100) NOT NULL UNIQUE,
   landlord_user_password char(255) NOT NULL,
-  FOREIGN KEY (landlord_user_id) REFERENCES users(user_id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO `landlord`(`landlord_user_id`, `landlord_user_first_name`,`landlord_user_last_name`,`landlord_user_email`,`landlord_user_password`) VALUES
-(1, "Bob", "Smith", "bobsmith@smithprop.com", "123"),
-(2, "Gill", "Corn", "gillcorn@cornprop.com", "123"),
-(3, "Billy", "Hour", "billyhour@hourprop.com", "123"),
-(4, "Michael", "Parker", "michaelparker@parkerprop.com", "123"),
-(5, "Seth", "Campbell", "sethcampbell@campbellprop.com", "123");
+INSERT INTO `landlord`(`landlord_user_id`, `landlord_user_first_name`,`landlord_user_last_name`,`landlord_user_email`,`landlord_user_password`, 'user_id') VALUES
+(1, "Bob", "Smith", "bobsmith@smithprop.com", "123", 1),
+(2, "Gill", "Corn", "gillcorn@cornprop.com", "123", 1),
+(3, "Billy", "Hour", "billyhour@hourprop.com", "123", 1),
+(4, "Michael", "Parker", "michaelparker@parkerprop.com", "123", 1),
+(5, "Seth", "Campbell", "sethcampbell@campbellprop.com", "123", 1);
 
 CREATE TABLE tenant (
   tenant_user_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -54,12 +54,12 @@ CREATE TABLE tenant (
 
 INSERT INTO `tenant`(`tenant_user_id`, `tenant_user_first_name`, `tenant_user_last_name`, `tenant_user_email`, `tenant_user_password`,) VALUES 
 (1, "Jif", "Silly", "jifsally@gmail.com", "123"),
-(2, "Jeff" "Jacobson", "jeffjacobson@gmail.com", "123"),
+(2, "Jeff", "Jacobson", "jeffjacobson@gmail.com", "123"),
 (3, "Sally", "Green", "sallygreen@gmail.com", "123"),
 (4, "May", "Clausen", "maycleusen@gmail.com", "123"),
 (5, "Alex", "Schiender", "alexsch@gmail.com", "123"),
 (6, "Alec", "Windsor", "alecwindsor@gmail.com", "123"),
-(7, "Alix", "Java", "alixjave@yahoo.com" "123"),
+(7, "Alix", "Java", "alixjave@yahoo.com", "123"),
 (8, "Jacob", "Pries", "jacobpries@gmail.com", "123"),
 (9, "Luke", "Brown", "lukebrown@gmail.com", "123"),
 (10, "Matt", "Smith", "mattsmith@gmail.com", "123"),
@@ -67,7 +67,7 @@ INSERT INTO `tenant`(`tenant_user_id`, `tenant_user_first_name`, `tenant_user_la
 (12, "Grace", "Wood", "gracewood@gmail.com", "123"),
 (13, "Carter", "Lockwood", "carterlockwood@gmail.com", "123"),
 (14, "Darth", "Vader", "darthvader@deathstar.com", "123"),
-(15, "Peter", "Walsh", "petrwalsh@england.com", "123"),
+(15, "Peter", "Walsh", "petrwalsh@england.com", "123");
 
 
 CREATE TABLE properties(
@@ -82,25 +82,26 @@ CREATE TABLE properties(
 );
 
 INSERT INTO `properties`(`property_id`, `property_address`, `property_city`, `property_state`, `property_zip_code`) VALUES 
-(1, ),
-(2,),
-(3,),
-(4,),
-(5,),
-(6,),
-(7,),
-(8,),
-(9,),
-(10,),
-(11,),
-(12,),
-(13,),
-(14,),
-(15,),
+(1, '123 Main St', "La Crosse", "WI", 54601),
+(2, '456 Elm St', "Miami", "FL", 33101),
+(3, '789 Oak St', "Miami", "FL", 33109),
+(4, '101 Pine St', "New York", "NY", 10001),
+(5, '202 Maple St', "Madison", "WI", 53558),
+(6, '303 Birch St', "Madison", "WI", 53562),
+(7, '404 Cedar St', "Madison", "WI", 53703),
+(8, '505 Spruce St', "Portland", "ME", 04101),
+(9, '606 Redwood St', "Portlan", "ME", 04102),
+(10, '707 Sequoia St', "Miami", "FL", 33102),
+(11, '808 Pine St', "Miami", "FL", 33102),
+(12, '909 Elm St', "Miami", "FL", 33102),
+(13, '1010 Oak St', "Miami", "FL", 33102),
+(14, '1111 Cedar St', "Miami", "FL", 33102),
+(15, '1212 Maple St', "Miami", "FL", 33102);
 
 CREATE TABLE rent_paid(
   rent_paid_id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   rent_paid_date DATE,
   FOREIGN KEY (properties) REFERENCES properties(property_id),
+  FOREIGN KEY (properties) REFERENCES properties(rent),
   rent_amount int(11) NOT NULL
 );
